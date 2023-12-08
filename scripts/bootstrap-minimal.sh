f46245248a9df8e59920dae1b29055135db8ef51
Sebastian Gnoyke <49392239+sgnoyke@users.noreply.github.com>
2023-12-08 11:32:18 +0100

    wip

--- scripts/bootstrap-minimal.sh
+++ scripts/bootstrap-minimal.sh
@@ -2,7 +2,7 @@
 
 if [ $EUID -eq 0 ]; then echo -e "${ERROR} This script should not be executed as root! Exiting ..."; exit 1; fi
 
-# Welcome message
+echo
 echo "${GREEN}Welcome to OpenSUSE (Tumbleweed) - Minimal Bootstrap Script!$(tput sgr0)"
 echo
 echo "${WARN} ATTENTION: All data on hardrive will be wiped!! $(tput sgr0)"
@@ -10,4 +10,4 @@ echo
 
 read -p "${BLUE}Would you like to proceed? (y/n): $(tput sgr0)" proceed
 
-if [ "$proceed" != "y" ]; then echo "Installation aborted."; exit 1; fi
+if [ "$proceed" != "y" ]; then echo "${WARN} Installation aborted."; exit 0; fi
